25bf73b9ff170cf6127014e7e7486c6d71f7bd1f
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2025-02-26 05:54:32 +0530

    validation

---
+++ validation.js
@@ -0,0 +1,13 @@
+import { validationResult } from "express-validator";
+
+const referralValidationRules = [
+    body('referrerName').notEmpty().withMessage('Referrer name is required'),
+    body('referrerEmail').isEmail().withMessage('Valid referrer email is required'),
+    body('referrerPhone').optional().isMobilePhone().withMessage('Valid referrer phone is required'),
+    body('refereeName').notEmpty().withMessage('Referee name is required'),
+    body('refereeEmail').isEmail().withMessage('Valid referee email is required'),
+    body('refereePhone').optional().isMobilePhone().withMessage('Valid referee phone is required'),
+    body('courseInterest').notEmpty().withMessage('Course interest is required'),
+  ];
+
+export default referralValidationRules
