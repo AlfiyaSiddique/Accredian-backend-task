56afd28c337d70c9e8a443575dad812b2e40a67b
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2025-02-26 19:44:01 +0530

    route fix

--- route.js
+++ route.js
@@ -10,6 +10,6 @@ referralRouter.post('/referrals', referralValidationRules, referral.createRefera
 // For these controllers I have added a postman collection to test and check due to lack of time to develop the frontend for this
 referralRouter.get('/referrals', referral.getReferral)
 referralRouter.get('/referrals/referrer/:email', referral.getReferralByReferrerEmail)
-referralRouter.patch('/api/referrals/:id/status', referral.updateReferralState)
+referralRouter.put('/referrals/:id/status', referral.updateReferralState)
 
 export default referralRouter
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: `${process.env.FRONTEND_URL}`, 
-  methods: "GET,POST,PUT,PATCH,DELETE",
+  methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization"
 }));
 
