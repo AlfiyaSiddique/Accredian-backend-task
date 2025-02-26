7d0ba036a983b4c50516da2a4136ceffba6b4200
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2025-02-26 19:38:16 +0530

    update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: `${process.env.FRONTEND_URL}`, 
-  methods: "GET,POST,PUT,DELETE",
+  methods: "GET,POST,PUT,PATCH,DELETE",
   allowedHeaders: "Content-Type,Authorization"
 }));
 
